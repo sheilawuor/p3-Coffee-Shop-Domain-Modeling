168775955e20d42fbb0452110d87ad759fb7e039
sheilawuor <sheilawuor01@gmail>
2025-11-28 18:00:25 +0300

    add files

--- coffee_shop/coffee.py
+++ coffee_shop/coffee.py
@@ -4,8 +4,20 @@ class Coffee:
         self._orders = []
     
     @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) < 3:
+            raise ValueError("Name must be a string at least 3 characters long")
+        self._name = value
+    
     def orders(self):
         return self._orders
+    
+    def customers(self):
+        return list(set(order.customer for order in self._orders))
     
     def add_order(self, order):
         self._orders.append(order)
--- coffee_shop/customer.py
+++ coffee_shop/customer.py
@@ -4,8 +4,20 @@ class Customer:
         self._orders = []
     
     @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 15):
+            raise ValueError("Name must be a string between 1 and 15 characters")
+        self._name = value
+    
     def orders(self):
         return self._orders
+    
+    def coffees(self):
+        return list(set(order.coffee for order in self._orders))
     
     def add_order(self, order):
         self._orders.append(order)
